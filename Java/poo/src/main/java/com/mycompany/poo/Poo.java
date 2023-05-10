db3c300a30c2f97bc3f220213fcd849ebc6e93d8
Gabriel da Silva Dornelles <130611852+gabrielnewforgis@users.noreply.github.com>
2023-05-09 21:38:47 -0300

    Add files via upload

--- Java/poo/src/main/java/com/mycompany/poo/Poo.java
+++ Java/poo/src/main/java/com/mycompany/poo/Poo.java
@@ -2,22 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 package com.mycompany.poo;
-
-import static com.mycompany.poo.User.deletar;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Poo {
 
     public static void main(String[] args) {
-        ArrayList<User> dados = new ArrayList<User>();
-
+        ArrayList<User> dados = new ArrayList<>();
+       User usuario = new User("", "", "","","", 0);
+         
         int op = 99;
 
         while (op != 7) {
-            String input = JOptionPane.showInputDialog(null, "Escolha uma opção:\n1-Cadastro\n2-Deletar\n7-Sair");
+            String input = JOptionPane.showInputDialog(null, "Escolha uma opção:\n1-Cadastro\n2-Deletar\n3-Credito\n4-Debito\n7-Sair");
             op = Integer.parseInt(input);
-
+ 
             switch (op) {
                 case 1 -> {
                     String nome = JOptionPane.showInputDialog(null, "Insira seu nome", "Cadastro", JOptionPane.PLAIN_MESSAGE);
@@ -25,14 +24,26 @@ public class Poo {
                     String idade = JOptionPane.showInputDialog(null, "Insira sua idade", "Cadastro", JOptionPane.PLAIN_MESSAGE);
                     String celular = JOptionPane.showInputDialog(null, "Insira seu número de celular", "Cadastro", JOptionPane.PLAIN_MESSAGE);
                     String id = JOptionPane.showInputDialog(null, "Insira seu ID", "Cadastro", JOptionPane.PLAIN_MESSAGE);
-                    User usuario = new User(nome, cpf, idade, celular, id, 0);
+                   usuario = new User(nome,cpf,idade,celular,id, 0);
                     dados.add(usuario);
                     JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!", "Cadastro", JOptionPane.PLAIN_MESSAGE);
                     JOptionPane.showMessageDialog(null, usuario.toString(), "Cadastro", JOptionPane.PLAIN_MESSAGE);
+                break;
                 }
                 case 2 -> {
                        String delete = JOptionPane.showInputDialog(null, "Insira o ID do usuário que deseja deletar", "Deletar", JOptionPane.PLAIN_MESSAGE);
-                deletar(dados, delete);
+                    usuario.deletar(dados,  delete);
+                break;
+                }
+                case 3 -> {
+                    String creditar = JOptionPane.showInputDialog(null, "Insira o ID do usuário", "Credito", JOptionPane.PLAIN_MESSAGE);
+                    usuario.credito(dados, creditar);
+                break;
+                }
+                case 4 -> {
+                    String debitar = JOptionPane.showInputDialog(null, "Insira o ID do usuário", "Debito", JOptionPane.PLAIN_MESSAGE);
+                    usuario.debito(dados, debitar);
+                break;
                 }
             }
         }
--- Java/poo/src/main/java/com/mycompany/poo/User.java
+++ Java/poo/src/main/java/com/mycompany/poo/User.java
@@ -88,20 +88,49 @@ public class User {
     }
 //Métodos
 
-    public static void deletar(ArrayList<User> dados, String id) {
+    public void deletar(ArrayList<User> dados, String id) {
+        boolean encontrado = false;
         for (User usuario : dados) {
             if (usuario.getId().equals(id)) {
+                encontrado = true;
                 int confirm = JOptionPane.showConfirmDialog(null, "Você realmente deseja deletar o usuário " + usuario.getNome() + "?", "Deletar", JOptionPane.YES_NO_OPTION);
-                 if (confirm == JOptionPane.YES_OPTION) {
-                dados.remove(usuario);
-                JOptionPane.showMessageDialog(null, "Usuário deletado com sucesso!", "Deletar", JOptionPane.PLAIN_MESSAGE);
-                return;
-                 }else{ 
-                     return;
-                 }
+                if (confirm == JOptionPane.YES_OPTION) {
+                    dados.remove(usuario);
+                    JOptionPane.showMessageDialog(null, "Usuário deletado com sucesso!", "Deletar", JOptionPane.PLAIN_MESSAGE);
+                    return;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Operação cancelada!", "Deletar", JOptionPane.ERROR_MESSAGE);
+                }
             }
         }
-    
-        JOptionPane.showMessageDialog(null, "Usuário não encontrado!", "Deletar", JOptionPane.PLAIN_MESSAGE);
+        if (!encontrado) {
+            JOptionPane.showMessageDialog(null, "Usuário não encontrado!", "Deletar", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void credito(ArrayList<User> dados, String id) {
+        for (User usuario : dados) {
+            if (usuario.getId().equals(id)) {
+                float credito = Float.parseFloat(JOptionPane.showInputDialog(null, "Saldo atual: " + usuario.getValor() + "\nInsira o valor a ser depositado", "Credito", JOptionPane.PLAIN_MESSAGE));
+                usuario.setValor(usuario.getValor() + credito);
+                int index = dados.indexOf(usuario);
+                dados.set(index, usuario);
+                JOptionPane.showMessageDialog(null, "Saldo atualizado com sucesso!\nSaldo atual: " + usuario.valor, "Credito", JOptionPane.PLAIN_MESSAGE);
+                return;
+            }
+        }
+    }
+
+    public void debito(ArrayList<User> dados, String id) {
+        for (User usuario : dados) {
+            if (usuario.getId().equals(id)) {
+                float debito = Float.parseFloat(JOptionPane.showInputDialog(null, "Saldo atual: " + usuario.getValor() + "\nInsira o valor a ser depositado", "Debito", JOptionPane.PLAIN_MESSAGE));
+                usuario.setValor(usuario.getValor() - debito);
+                int index = dados.indexOf(usuario);
+                dados.set(index, usuario);
+                JOptionPane.showMessageDialog(null, "Saldo atualizado com sucesso!\nSaldo atual: " + usuario.valor, "Debito", JOptionPane.PLAIN_MESSAGE);
+                return;
+            }
+        }
     }
 }
